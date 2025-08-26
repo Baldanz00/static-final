bf7ac411e49b82fdda9ccaa81d320a0a9b0636fd
Baldanz00 <gioia.travaglini18@gmail.com>
2025-08-26 17:31:12 +0200

    BigDecimal e CalcoliHelper

--- src/calcoliHelper/UsaCalcoliHeper.java
+++ src/calcoliHelper/UsaCalcoliHeper.java
@@ -1,5 +1,8 @@
 package calcoliHelper;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class UsaCalcoliHeper {
 
 
@@ -9,9 +12,23 @@ public class UsaCalcoliHeper {
 
         System.out.println("Somma " + somma + " prodotto " + prodotto);
 
-        double divisione = CalcoliHelper.divisione(1, 3);
+        double divisione = CalcoliHelper.divisione(1.2, 3.3);
         System.out.println("Divisione " + divisione);
 
-        
+        /* Se io metto 1.2 e 3.3 escono numero particolari, perchè il double , essendo un numero a virgola mobile,
+         * dopo un po perde delle informazioni.... più aumenta il numero dopo la virgola, più l'errore può essere significativo
+         * Come evito questi errori nei calcoli? 
+         * BigDecimal -> oggetto java pensato per lavorare a oggetti
+         * 
+         * |_ vuole un valore di inizio, che si passano a stringa e lui li converte da solo
+         */
+        BigDecimal dividento = new BigDecimal("1.21");
+        BigDecimal divisore = new BigDecimal("3.32");
+
+        /* BigDecimal è un oggetto non modificabile, ogni operazione restituisce una nuova istanta di BD */
+       System.out.println( dividento.add(divisore));
+
+       /* Ha bisogno di sapere quale sistema di arrotondamento usare */
+       System.out.println("Divisione: " + dividento.divide(divisore, 4, RoundingMode.HALF_UP));
     }
 }
